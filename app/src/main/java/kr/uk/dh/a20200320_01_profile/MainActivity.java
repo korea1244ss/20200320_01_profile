75401a03e417ca1806b0223760937ecf4dfd1116
korea1244ss <61854931+korea1244ss@users.noreply.github.com>
2020-03-20 10:16:40 +0900

    데이타 바인딩 준비~!

--- app/src/main/java/kr/uk/dh/a20200320_01_profile/MainActivity.java
+++ app/src/main/java/kr/uk/dh/a20200320_01_profile/MainActivity.java
@@ -1,18 +1,34 @@
 package kr.uk.dh.a20200320_01_profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import kr.uk.dh.a20200320_01_profile.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        setupEvets();
+        setValues();
+
+    }
+
+
+    @Override
+    public void setupEvets() {
+
+    }
+
+    @Override
+    public void setValues() {
+
     }
 }
