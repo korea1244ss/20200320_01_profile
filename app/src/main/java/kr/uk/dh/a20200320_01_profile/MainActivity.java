a397ff211efa60060d3b6baa10cfef4b0c09e9af
korea1244ss <61854931+korea1244ss@users.noreply.github.com>
2020-03-20 11:17:14 +0900

    선택한 이미지 경로 로그로 찍기까지.

--- app/src/main/java/kr/uk/dh/a20200320_01_profile/MainActivity.java
+++ app/src/main/java/kr/uk/dh/a20200320_01_profile/MainActivity.java
@@ -1,14 +1,20 @@
 package kr.uk.dh.a20200320_01_profile;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 
 import kr.uk.dh.a20200320_01_profile.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
+
+    private static final int REQUEST_FOR_ALBUM = 1000;
 
     ActivityMainBinding binding = null;
 
@@ -31,11 +37,42 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvets() {
+        binding.profilePicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+//                갤러리에서 사진을 가져오자~!
+//                안드로이다가 제공 하는 화면(갤러리) => Intent로 이동 하자.!
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent,REQUEST_FOR_ALBUM);
+//                숫자로 했을시 먼지 모르니 이름을 부여 해서 변경
+//                MediaStore < 자동완성 Content Type 자동 완성 해야함...
+
+            }
+        });
     }
 
     @Override
     public void setValues() {
 
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_FOR_ALBUM) {
+            if (resultCode == RESULT_OK) {
+               if (data.getData() != null) {
+                   Log.d("사진가져오기",data.getDataString().toString());
+//                   제대로 작동 안함 ㅋㅋㅋ
+               }
+            }
+        }
     }
 }
